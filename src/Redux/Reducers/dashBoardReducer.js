7091b0c92ff97eb8427a47ddebbfc4df30fc85ea
Muskan Rawal <rawalmuskan298@gmail.com>
2022-02-25 13:41:23 +0530

    dashboard route fix

--- src/Redux/Reducers/dashBoardReducer.js
+++ src/Redux/Reducers/dashBoardReducer.js
@@ -5,8 +5,8 @@ import {
   SET_DASHBOARD_DATA,
   SET_DATE_RANGE,
   SET_LOADING_DATA,
-  SET_LOADING_DATE
-} from '../Actions/ActionTypes';
+  SET_LOADING_DATE,
+} from "../Actions/ActionTypes";
 
 const initialState = {
   dateRange: [],
@@ -17,7 +17,7 @@ const initialState = {
   barData: [],
   pieData: [],
   loadingData: false,
-  redirectTo: ''
+  redirectTo: "/",
 };
 
 const dashboardReducer = (state = initialState, action) => {
@@ -25,24 +25,24 @@ const dashboardReducer = (state = initialState, action) => {
     case SET_DATE_RANGE:
       return {
         ...state,
-        dateRange: action.payload
+        dateRange: action.payload,
       };
     case FETCH_VALID_DATE_RANGE:
       return {
         ...state,
         loadingDate: false,
         minDate: action.payload.startDate,
-        maxDate: action.payload.endDate
+        maxDate: action.payload.endDate,
       };
     case SET_LOADING_DATE:
       return {
         ...state,
-        loadingDate: true
+        loadingDate: true,
       };
     case SET_LOADING_DATA:
       return {
         ...state,
-        loadingData: true
+        loadingData: true,
       };
     case SET_DASHBOARD_DATA:
       return {
@@ -50,12 +50,12 @@ const dashboardReducer = (state = initialState, action) => {
         loadingData: false,
         tableData: action.payload[0].data.result.data,
         barData: action.payload[1].data.result.data,
-        pieData: action.payload[2].data.result.data
+        pieData: action.payload[2].data.result.data,
       };
     case REDIRECT:
       return {
         ...state,
-        redirectTo: action.payload
+        redirectTo: action.payload,
       };
     case LOGOUT:
       return initialState;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,31 @@
-import { Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { fetchDashboardData, fetchValidDateRange } from '../Redux/Actions/DashBoardAction';
-import BarData from './BarData';
-import BasicDateRangePicker from './DateRange';
-import Loader from './Loader';
-import PieData from './PieData';
-import TableData from './TableData';
+import { Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  fetchDashboardData,
+  fetchValidDateRange,
+} from "../Redux/Actions/DashBoardAction";
+import BarData from "./BarData";
+import BasicDateRangePicker from "./DateRange";
+import Loader from "./Loader";
+import PieData from "./PieData";
+import TableData from "./TableData";
 
 const useStyles = makeStyles(() => ({
   btn: {
-    background: 'blue',
-    color: 'white',
-    marginTop: '1em'
+    background: "blue",
+    color: "white",
+    marginTop: "1em",
   },
   datePicker: {
-    margin: '3em',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    flexDirection: 'column'
-  }
+    margin: "3em",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+  },
 }));
 
 function Dashboard() {
@@ -39,13 +42,17 @@ function Dashboard() {
     pieData,
     redirectTo,
     minDate,
-    maxDate
+    maxDate,
   } = useSelector((state) => state.dashboard);
 
   const { userData } = useSelector((state) => state.user);
 
   useEffect(() => {
-    if (userData) {
+    debugger;
+    if (!userData) {
+      dispatch({ type: "LOGOUT" });
+      // navigate("/");
+    } else {
       dispatch(fetchValidDateRange());
     }
   }, []);
@@ -55,34 +62,42 @@ function Dashboard() {
   }, [redirectTo]);
 
   const handleLogout = () => {
-    dispatch({ type: 'LOGOUT' });
-    navigate('/');
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
   };
 
   const handleData = () => {
-    let startDate = dateRange[0] ? '' + dateRange[0].valueOf() : minDate;
-    let endDate = dateRange[1] ? '' + dateRange[1].valueOf() : maxDate;
+    let startDate = dateRange[0] ? "" + dateRange[0].valueOf() : minDate;
+    let endDate = dateRange[1] ? "" + dateRange[1].valueOf() : maxDate;
     dispatch(fetchDashboardData(startDate, endDate));
   };
 
   return (
     <>
       {loadingDate && !dateRange.length ? (
-        <Loader text={'LOADING DASHBOARD'} />
+        <Loader text={"LOADING DASHBOARD"} />
       ) : (
         <div>
-          <Button variant="contained" onClick={handleLogout} className={classes.btn}>
+          <Button
+            variant="contained"
+            onClick={handleLogout}
+            className={classes.btn}
+          >
             Logout
           </Button>
           <div className={classes.datePicker}>
             <BasicDateRangePicker />
             <br />
-            <Button className={classes.btn} variant="contained" onClick={handleData}>
+            <Button
+              className={classes.btn}
+              variant="contained"
+              onClick={handleData}
+            >
               SHOW DATA
             </Button>
           </div>
           {loadingData ? (
-            <Loader text={'LOADING DATA'} />
+            <Loader text={"LOADING DATA"} />
           ) : (
             <>
               {tableData.length > 0 && <TableData tableData={tableData} />}
